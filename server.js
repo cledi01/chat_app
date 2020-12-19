0da7de27086fa9b96d040797fe22772dd7e2c987
cledi01 <caleb.gorrila@gmail.com>
2020-12-19 11:01:33 +0700

    Prepare code for deployment

---
+++ server.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require('apollo-server');
+
+require('dotenv').config();
+
+const { sequelize } = require('./models');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const contextMiddleware = require('./util/contextMiddleware');
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: contextMiddleware,
+	subscriptions: { path: '/' },
+});
+
+server.listen().then(({ url, subscriptionsUrl }) => {
+	console.log(`🚀 Server ready at ${url}`);
+	console.log(`🚀 Subscription ready at ${subscriptionsUrl}`);
+	sequelize
+		.authenticate()
+		.then(() => console.log('Database connected!'))
+		.catch((err) => console.log(err));
+});
